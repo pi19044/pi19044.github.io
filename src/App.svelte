103eb37979ae1709c178093fd5fe14b300ea2f09
PrelevatedInsider19044 <membermadde.insiddev25120@hotmail.com>
2024-07-16 22:44:53 +0530

    ee

---
+++ src/App.svelte
@@ -0,0 +1,106 @@
+<script>
+	import * as Fluent from "fluent-svelte";
+	import "fluent-svelte/theme.css";
+	import { base } from '$app/paths';
+</script>
+
+<style>
+
+	nav {
+		display: flex;
+		justify-content: left;
+		align-items: center;
+		border-bottom: 2px solid #333;
+		padding: 8px;
+	}
+
+	h1, h2, h3, h4, h5, h6, span, p, nav a {
+		font-family: 'Segoe UI', system-ui, -apple-system, BlinkMacSystemFont, Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
+	}
+
+	h1 {
+		font-weight: 500;
+		transition: 1s ease-in-out;
+		animation: ease-in-out font-weight;
+	}
+
+	h1:hover {
+		font-weight: 700;
+		text-transform: uppercase;
+		letter-spacing: 10px;
+	}
+
+	nav a {
+		text-decoration: none;
+		color: white;
+		margin-right: 14px !important;
+	}
+
+	nav a.logo img {
+		width: 30px;
+		height: 30px;
+		border-radius: 100%;
+		margin-top: 5px;
+		margin-left: 5px;
+		margin-right: 5px;
+	}
+
+	main {
+		display: block;
+		text-align: center;
+		padding: 85px 10px;
+	}
+
+	main * {
+		display: block;
+	}
+
+	main h1 {
+		margin-top: 0;
+	}
+	
+	footer {
+		padding-top: 10px;
+		padding-bottom: 10px;
+		padding-left: 25px;
+		background: var(--fds-solid-background-base);
+		border-top: 1px solid rgba(205, 205, 205, 0.25);
+		display: flex;
+		flex-direction: column;
+		align-items: flex-start;
+		position: absolute;
+		bottom: 0;
+		width: calc(100vw - 25px);
+	}
+
+	@keyframes font-weight {
+		0% {
+			font-weight: 500;
+		}
+		25% {
+			font-weight: 600;
+		}
+		100% {
+			font-weight: 700;
+		}
+	}
+</style>
+
+<div>
+	<nav>
+		<a href="/" class="logo"><img src="https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fimages.wallpapersden.com%2Fimage%2Fdownload%2Flofoten-norway_bGdlbGuUmZqaraWkpJRobWllrWdma2U.jpg&f=1&nofb=1&ipt=02df2dad3d3484a9cc57c13c9b2c065f1bdd44710fe34cab98dee464f5c8a884&ipo=images" alt="Xandaland's pfp"></a>
+		<Fluent.Button href="{base}/" style="margin-right: 14px;">Home</Fluent.Button>
+		<Fluent.Button href="{base}/plans" style="margin-right: 14px;" variant="hyperlink">Plans</Fluent.Button>
+		<Fluent.Button href="{base}/contact" variant="hyperlink">Contact me</Fluent.Button>
+	</nav>
+	<main>
+		<h1>Welcome to my website</h1>
+		<p>Hello, I'm Xandaland (a.k.a. pi19044), I like to write code.</p>
+		<Fluent.InfoBar style="max-width: 768px; margin: 0 auto; text-align: left" severity="caution" title="Important!">This website is under construction. It's my first time making an website with Svelte. Thank you for your patience.</Fluent.InfoBar>
+	</main>
+	<footer>
+		<h5 style="margin: 0px; margin-top: 10px">Powered by:</h5>
+		<Fluent.Button href="https://kit.svelte.dev" variant="hyperlink" style="margin-top: 10px; margin-bottom: 10px">SvelteKit</Fluent.Button>
+		<Fluent.Button href="https://fluent-svelte.vercel.app/" variant="hyperlink" style="margin-bottom: 10px">Fluent Svelte</Fluent.Button>
+	</footer>
+</div>
---
+++ src/contact.svelte
@@ -0,0 +1,102 @@
+<script>
+	import * as Fluent from "fluent-svelte";
+	import "fluent-svelte/theme.css";
+	import { base } from '$app/paths';
+</script>
+
+<style>
+
+	nav {
+		display: flex;
+		justify-content: left;
+		align-items: center;
+		border-bottom: 2px solid #333;
+		padding: 8px;
+	}
+
+	h1, h2, h3, h4, h5, h6, span, p, nav a {
+		font-family: 'Segoe UI', system-ui, -apple-system, BlinkMacSystemFont, Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
+	}
+
+	h1 {
+		font-weight: 500;
+		transition: 1s ease-in-out;
+		animation: ease-in-out font-weight;
+	}
+
+	nav a {
+		text-decoration: none;
+		color: white;
+		margin-right: 14px;
+	}
+
+	nav a.logo img {
+		width: 30px;
+		height: 30px;
+		border-radius: 100%;
+		margin-top: 5px;
+		margin-left: 5px;
+		margin-right: 5px;
+	}
+
+	main {
+		display: block;
+		padding: 85px 65px;
+	}
+
+	main * {
+		display: block;
+	}
+
+	main h1, main h2 {
+		margin-top: 0;
+	}
+
+	footer {
+		padding-top: 10px;
+		padding-bottom: 10px;
+		padding-left: 25px;
+		background: var(--fds-solid-background-base);
+		border-top: 1px solid rgba(205, 205, 205, 0.25);
+		display: flex;
+		flex-direction: column;
+		align-items: flex-start;
+		position: absolute;
+		bottom: 0;
+		width: calc(100vw - 25px);
+	}
+
+	@keyframes font-weight {
+		0% {
+			font-weight: 500;
+		}
+		25% {
+			font-weight: 600;
+		}
+		100% {
+			font-weight: 700;
+		}
+	}
+</style>
+
+<div>
+	<nav>
+		<a href="{base}/" class="logo"><img src="https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fimages.wallpapersden.com%2Fimage%2Fdownload%2Flofoten-norway_bGdlbGuUmZqaraWkpJRobWllrWdma2U.jpg&f=1&nofb=1&ipt=02df2dad3d3484a9cc57c13c9b2c065f1bdd44710fe34cab98dee464f5c8a884&ipo=images" alt="Xandaland's pfp"></a>
+		<Fluent.Button href="{base}/" style="margin-right: 14px;" variant="hyperlink">Home</Fluent.Button>
+		<Fluent.Button href="{base}/plans" style="margin-right: 14px;" variant="hyperlink">Plans</Fluent.Button>
+		<Fluent.Button href="{base}/contact">Contact me</Fluent.Button>
+	</nav>
+	<main>
+		<h1>Contact me</h1>
+		<p>You can contact me on these:</p>
+        <Fluent.Button onClick="window.open('https://twitter.com/PrelevatedIns', '_blank')">Twitter</Fluent.Button>
+		<Fluent.Button onClick="window.open('mailto:membermadde.insiddev25120@hotmail.com', '_blank')">Hotmail</Fluent.Button>
+		<Fluent.Button onClick="window.open('https://github.com/pi19044', '_blank')">GitHub</Fluent.Button>
+		<Fluent.Button onClick="window.open('https://www.guilded.gg/u/xandaland', '_blank')">Guilded</Fluent.Button>
+	</main>
+	<footer>
+		<h5 style="margin: 0px; margin-top: 10px">Powered by:</h5>
+		<Fluent.Button href="https://kit.svelte.dev" variant="hyperlink" style="margin-top: 10px; margin-bottom: 10px">SvelteKit</Fluent.Button>
+		<Fluent.Button href="https://fluent-svelte.vercel.app/" variant="hyperlink" style="margin-bottom: 10px">Fluent Svelte</Fluent.Button>
+	</footer>
+</div>
