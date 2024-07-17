cec45fdbcaa8db6d2b8840f0337e41b42b90ac56
PrelevatedInsider19044 <membermadde.insiddev25120@hotmail.com>
2024-07-17 09:51:58 +0530

    Update svelte.config.js

--- svelte.config.js
+++ svelte.config.js
@@ -1,27 +1,13 @@
 import adapter from '@sveltejs/adapter-static';
-import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-	// Consult https://kit.svelte.dev/docs/integrations#preprocessors
-	// for more information about preprocessors
-	preprocess: vitePreprocess(),
-
 	kit: {
-		// adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
-		// If your environment is not supported, or you settled on a specific environment, switch out the adapter.
-		// See https://kit.svelte.dev/docs/adapters for more information about adapters.
-		adapter: adapter(),
+		adapter: adapter({
+			fallback: '404.html'
+		}),
 		paths: {
-            base: process.env.NODE_ENV === 'production' ? '/sveltekit-github-pages' : '',
-        },
-		prerender: {
-		  	handleHttpError: ({ status, path, referrer, referenceType }) => {
-				if (status === 404 && path.startsWith('/sveltekit-github-pages')) {
-			  		return;
-				}
-				throw new Error(`${status} ${path} does not begin with base path`);
-		  	}
+			base: process.argv.includes('dev') ? '' : process.env.BASE_PATH
 		}
 	}
 };
