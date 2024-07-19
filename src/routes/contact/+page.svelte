2a3c74064bac8013ab6ad43ca1f8ee9ff6eceeb6
PrelevatedInsider19044 <membermadde.insiddev25120@hotmail.com>
2024-07-19 20:51:56 +0530

    Update +page.svelte

--- src/routes/contact/+page.svelte
+++ src/routes/contact/+page.svelte
@@ -96,7 +96,7 @@
         	<Fluent.Button onClick="window.open('https://twitter.com/PrelevatedIns', '_blank')">Twitter</Fluent.Button>
 		<Fluent.Button onClick="window.open('mailto:membermadde.insiddev25120@hotmail.com', '_blank')">Hotmail</Fluent.Button>
 		<Fluent.Button onClick="window.open('https://github.com/pi19044', '_blank')">GitHub</Fluent.Button>
-		<Fluent.Button onClick="window.open('https://www.guilded.gg/xandaland', '_blank')">Guilded</Fluent.Button>
+		<Fluent.Button onClick="window.open('https://discord.gg/darwQ3ZMXX', '_blank')">Discord</Fluent.Button>
 	</main>
 	<footer>
 		<h5 style="margin: 0px; margin-top: 10px">Powered by:</h5>
