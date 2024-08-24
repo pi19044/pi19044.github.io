92c285bc963ada5148ab85612ee0860359913d3a
PrelevatedInsider19044 <membermadde.insiddev25120@hotmail.com>
2024-08-24 17:23:00 +0530

    Update +page.svelte

--- src/routes/troll/+page.svelte
+++ src/routes/troll/+page.svelte
@@ -87,6 +87,7 @@
 		<Fluent.Button href="/contact" variant="hyperlink">Contact me</Fluent.Button>
 	</nav>
 	<main>
-		<p>WINDOWS MADARCHOD, IF YOU ARE SEEING THIS, I GOT YOUR IP AND YOU ARE ABOUT TO BE DEAD MEAT</p>
+		<p>WINDOWS 8 MADARCHOD, IF YOU ARE SEEING THIS, I GOT YOUR IP AND YOU ARE ABOUT TO BE DEAD MEAT</p>
+        </main>
   </main>
 </div>
